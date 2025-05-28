8b2720e68b3a7a3427e62bbf850cf672f7260918
João Medeiros <jpmdrsdev@gmail.com>
2025-05-27 22:44:53 -0300

    feat: creating statistics controller

---
+++ src/main/java/com/jpmdrs/transaction_statistics_api/controller/StatisticsController.java
@@ -0,0 +1,28 @@
+package com.jpmdrs.transaction_statistics_api.controller;
+
+import com.jpmdrs.transaction_statistics_api.dto.StatisticsResponse;
+import com.jpmdrs.transaction_statistics_api.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.DoubleSummaryStatistics;
+
+@RestController
+@RequestMapping("/estatistica")
+public class StatisticsController {
+
+    private final TransactionService transactionService;
+
+    public StatisticsController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<StatisticsResponse> getStatistics() {
+        DoubleSummaryStatistics stats = transactionService.getStatistics();
+        return ResponseEntity.ok(new StatisticsResponse(stats));
+    }
+
+}
